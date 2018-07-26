3fef37a226e8fcab66daa6bcdf5598114db0d250
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-07-26 16:52:07 +0530

    Create number.java

---
+++ number.java
@@ -0,0 +1,17 @@
+# guvi5
+import java.util.Scanner;
+class Positive
+{
+  public static void main(String[] args)
+  {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter a number");
+    Long number=sc.nextLong();
+    if(number>0)
+      System.out.println("Positive");
+     else if(number<0)
+      System.out.println("Negative");
+     else
+      System.out.println("Zero");
+     }
+   }
