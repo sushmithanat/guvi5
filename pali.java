b468bd61d2e6f2032013c62c7e42c93d54266ab4
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-07-27 16:16:12 +0530

    Update and rename power.java to pali.java

---
+++ pali.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class pali
+{
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		int num=sc.nextInt();
+		int dup1=num,dup=0,rem=0;
+		while(num>0)
+		{
+			rem=num%10;
+			dup=dup*10+rem;
+			num=num/10;
+		}
+		if(dup1==dup)
+			System.out.println("yes");
+		else
+			System.out.println("no");	
+		
+	}
+}
