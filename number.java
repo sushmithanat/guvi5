178965d3f7ac3fb85bf8a38dbae6b75599fae5b7
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-07-27 10:00:25 +0530

    Update number.java

--- number.java
+++ number.java
@@ -4,7 +4,6 @@ class Positive
   public static void main(String[] args)
   {
     Scanner sc=new Scanner(System.in);
-    System.out.println("Enter a number");
     long number=sc.nextLong();
     if(number>0)
       System.out.println("Positive");
