117caabaad4ba6e06d29247e6152d80c18c0a9b3
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-07-27 10:43:54 +0530

    Update number.java

--- number.java
+++ number.java
@@ -1,15 +1,15 @@
 import java.util.Scanner;
-class Positive
+class number
 {
   public static void main(String[] args)
   {
     Scanner sc=new Scanner(System.in);
-    long number=sc.nextLong();
-    if(number>0)
+    long num=sc.nextLong();
+    if(num>0)
       System.out.println("Positive");
-     else if(number<0)
-      System.out.println("Negative");
-     else
+    else if(num<0)
+      System.out.println("Positive");
+    else
       System.out.println("Zero");
-     }
-   }
+  }
+}
