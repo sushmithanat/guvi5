f90b6ad2283fefd00901cc519f3e95ec555ced6d
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-07-29 14:45:20 +0530

    Update and rename sum2.java to digitssum.java

--- digitssum.java
+++ digitssum.java
@@ -5,14 +5,19 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class sum2
+class digitssum
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
-		int n1=sc.nextInt();
-		int n2=sc.nextInt();
-		int sum=n1+n2;
+		int n=sc.nextInt();
+		int rem=0,sum=0;
+		while(n>0)
+		{
+			rem=n%10;
+			sum=sum+rem;
+			n /= 10;
+		}
 		System.out.println(sum);
 	}
 }
