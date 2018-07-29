4d0d88f028e735ab814787f915b9aaae23a68ad8
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-07-29 14:59:37 +0530

    Update and rename producteven.java to search.java

--- search.java
+++ search.java
@@ -5,17 +5,29 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class producteven
+class search
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
-		int n1=sc.nextInt();
-		int n2=sc.nextInt();
-		int product=n1*n2;
-		if(product%2==0)
-		System.out.println("even");
-		else
-		System.out.println("odd");
+		int n=sc.nextInt();
+		int k=sc.nextInt();
+		int[] a=new int[n];
+		int i=0;
+		for(i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		for(i=0;i<n;i++)
+		{
+			if(a[i]==k)
+			{
+			System.out.println("yes");
+			break;
+			}
+		}
+		if(i==n)
+		System.out.println("no");
+		
 	}
 }
