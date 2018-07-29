5226d9fbf030220744bd2ec05b08926eb4282344
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-07-29 14:36:53 +0530

    Update and rename average.java to sum2.java

--- sum2.java
+++ sum2.java
@@ -5,23 +5,14 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class averge
+class sum2
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		int[] a=new int[n];
-		for(int i=0;i<n;i++)
-		{
-			a[i]=sc.nextInt();
-		}
-		int sum=0;
-		for(int i=0;i<n;i++)
-		{
-			sum += a[i];
-		}
-		sum /=n;
+		int n1=sc.nextInt();
+		int n2=sc.nextInt();
+		int sum=n1+n2;
 		System.out.println(sum);
 	}
 }
