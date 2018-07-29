63e015bb0257fb5e275335040c8d871a53c34125
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-07-29 17:46:52 +0530

    Update and rename search.java to number.java

--- number.java
+++ number.java
@@ -5,29 +5,19 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class search
+class number
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		int k=sc.nextInt();
-		int[] a=new int[n];
-		int i=0;
-		for(i=0;i<n;i++)
-		{
-			a[i]=sc.nextInt();
-		}
-		for(i=0;i<n;i++)
-		{
-			if(a[i]==k)
-			{
+		String str=sc.nextLine();
+		if(isNumeric(str))
 			System.out.println("yes");
-			break;
-			}
-		}
-		if(i==n)
-		System.out.println("no");
-		
+		else
+			System.out.println("no");
+	}
+	public static boolean isNumeric(String str)
+	{
+		return str.matches("-?\\d+(\\.\\d+)?");
 	}
 }
