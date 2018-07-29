dfa01d605e53bfb68513cd6f9051c6e8ca1a25f1
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-07-29 14:33:00 +0530

    Update and rename increment.java to average.java

--- average.java
+++ average.java
@@ -5,13 +5,23 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class increment
+class averge
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
-		n++;
-		System.out.println(n);
+		int[] a=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		int sum=0;
+		for(int i=0;i<n;i++)
+		{
+			sum += a[i];
+		}
+		sum /=n;
+		System.out.println(sum);
 	}
 }
