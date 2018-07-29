ea410441d4e4af76dccef7a5183e6ffddcb6ebad
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-07-29 11:04:04 +0530

    Update and rename prime.java to odd.java

---
+++ odd.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class odd
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int q=sc.nextInt();
+		for(int i=n+1;i<q;i++)
+		{
+			if(i%2!=0)
+				System.out.print(i+" ");
+		}
+	}
+}
