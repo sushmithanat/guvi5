89b1577d9a74c237646f2b42189b09c140b553f4
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-07-29 14:17:07 +0530

    Update and rename factorial.java to example.java

---
+++ example.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class example
+{
+	public static void main (String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		int k=sc.nextInt();
+		for(int i=0;i<k;i++)
+			System.out.println(s);
+	}
+}
