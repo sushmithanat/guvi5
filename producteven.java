799192c636591f19853c23f8fa2be8f0c1880716
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-07-29 14:49:38 +0530

    Update and rename digitssum.java to producteven.java

--- producteven.java
+++ producteven.java
@@ -5,19 +5,17 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class digitssum
+class producteven
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		int rem=0,sum=0;
-		while(n>0)
-		{
-			rem=n%10;
-			sum=sum+rem;
-			n /= 10;
-		}
-		System.out.println(sum);
+		int n1=sc.nextInt();
+		int n2=sc.nextInt();
+		int product=n1*n2;
+		if(product%2==0)
+		System.out.println("even");
+		else
+		System.out.println("odd");
 	}
 }
