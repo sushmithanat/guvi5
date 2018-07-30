25ebfb652489853dc7a3f4db9eb5857d2788e845
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-07-30 21:42:01 +0530

    Update and rename linecnt.java to intcnt.java

--- intcnt.java
+++ intcnt.java
@@ -4,27 +4,18 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class linecnt
+class intcnt
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
 		String str=sc.nextLine();
-		int count=1;
-		for(int i=1;i<str.length();i++)
+		int count=0;
+		for(int i=0;i<str.length();i++)
 		{
-			if(str.charAt(i)=='.' && str.charAt(i+1)==' ')
-			{
-				count++;	
-			}
-			else if(str.charAt(i)==' ' && str.charAt(i+1)==' ')
-			{
-				count++;
-			}
-			else if(str.charAt(i)=='.')
-			{
-				count++;
-			}
+			if(str.charAt(i)=='1' || str.charAt(i)=='2'
+|| str.charAt(i)=='3' || str.charAt(i)=='4' || str.charAt(i)=='5' || str.charAt(i)=='6' || str.charAt(i)=='7' || str.charAt(i)=='8' || str.charAt(i)=='9' || str.charAt(i)=='0')
+			count++;
 		}
 		System.out.println(count);
 	}
