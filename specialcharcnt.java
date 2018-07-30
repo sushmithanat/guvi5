e8770a939120c7c7ea1811ef84eb78e23b7276a1
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-07-30 22:37:09 +0530

    Update and rename intcnt.java to specialcharcnt.java

--- specialcharcnt.java
+++ specialcharcnt.java
@@ -4,18 +4,27 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class intcnt
+class specialcharcnt
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
 		String str=sc.nextLine();
-		int count=0;
-		for(int i=0;i<str.length();i++)
+		int count=0,i=0;
+		for(i=0;i<str.length();i++)
 		{
-			if(str.charAt(i)=='1' || str.charAt(i)=='2'
-|| str.charAt(i)=='3' || str.charAt(i)=='4' || str.charAt(i)=='5' || str.charAt(i)=='6' || str.charAt(i)=='7' || str.charAt(i)=='8' || str.charAt(i)=='9' || str.charAt(i)=='0')
-			count++;
+			int ch=str.charAt(i);
+			if(ch>=33 && ch<=47)
+				count++;
+			else if(ch>=58 && ch<=64)
+				count++;
+			else if(ch>=91 && ch<=96)
+				count++;
+			else if(ch>=123 && ch<=127)
+				count++;
+			else
+			{
+			}
 		}
 		System.out.println(count);
 	}
