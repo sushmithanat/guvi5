bb14620e40c6b95e2aede268f7aaf6364042c643
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-07-30 21:03:39 +0530

    Update and rename spacecnt.java to wrdcnt.java

--- wrdcnt.java
+++ wrdcnt.java
@@ -4,13 +4,13 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class spacecnt
+class wrdcnt
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
 		String str=sc.nextLine();
-		int count=0;
+		int count=1;
 		for(int i=1;i<str.length();i++)
 		{
 			if(str.charAt(i)==' ')
