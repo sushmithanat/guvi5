a59b83b815abdf1d8e5b179f128d80ad8bb5debf
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-07-30 20:56:31 +0530

    Update and rename strcount.java to spacecnt.java

--- spacecnt.java
+++ spacecnt.java
@@ -1,25 +1,22 @@
 /* package whatever; // don't place package name! */
-
 import java.util.*;
 import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class strcount
+class spacecnt
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
 		String str=sc.nextLine();
 		int count=0;
-		for(int i=0;i<str.length();i++)
+		for(int i=1;i<str.length();i++)
 		{
-			if(str.charAt(i)==' ' && str.charAt(i+1)!=' ')
+			if(str.charAt(i)==' ')
 			{
-				
+				count++;	
 			}
-			else
-			count++;
 		}
 		System.out.println(count);
 	}
