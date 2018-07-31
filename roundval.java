9a5c4e0e6e077ca9f12f29b7ec039ec1c2b09b02
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-07-31 16:35:14 +0530

    Create roundval.java

---
+++ roundval.java
@@ -0,0 +1,18 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class roundval
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		float f=sc.nextFloat();
+		int i=(int)f;
+		i += 1;
+		System.out.println(i);
+	}
+}
