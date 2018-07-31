94b156df9bb2a1920bda93403ddeebcf41e35ecc
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-07-31 09:52:57 +0530

    Update and rename swapnum.java to swapbit.java

--- swapbit.java
+++ swapbit.java
@@ -5,16 +5,16 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class swapnum
+class swapbit
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
 		int num1=sc.nextInt();
 		int num2=sc.nextInt();
-		int tmp=num1;
-		num1=num2;
-		num2=tmp;
+		num1= num1 ^ num2;
+		num2= num1 ^ num2;
+		num1= num1 ^ num2;
 		System.out.print(num1+" "+num2);
 	}
 }
