8b6fed51881fdf42b031a3cce99a72893e0af95d
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-07-31 16:44:09 +0530

    Update and rename roundval.java to compsite.java

--- compsite.java
+++ compsite.java
@@ -5,14 +5,21 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class roundval
+class compsite
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
-		float f=sc.nextFloat();
-		int i=(int)f;
-		i += 1;
-		System.out.println(i);
+		int n=sc.nextInt();
+		int i,count=0;
+		for(i=1;i<=n;i++)
+		{
+			if(n%i==0)
+				count++;	
+		}
+		if(count==2)
+			System.out.println("no");
+		else
+			System.out.println("yes");
 	}
 }
