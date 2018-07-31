f0a3924cb588f8cb289da8b911acdbc506040bf7
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-07-31 12:28:39 +0530

    Update and rename sumeven.java to sumeven1.java

--- sumeven1.java
+++ sumeven1.java
@@ -5,7 +5,7 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class sumeven
+class sumeven1
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
@@ -16,7 +16,7 @@ class sumeven
 		if(sum%2==0)
 			System.out.println("even");
 		else
-			System.out.println("no");
+			System.out.println("odd");
 		
 			
 	}
