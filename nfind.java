a1beb1235760d787e835d13b833c95d2f8a8fa53
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-07-31 15:54:47 +0530

    Update and rename findn1.java to nfind.java

--- nfind.java
+++ nfind.java
@@ -5,7 +5,7 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class find1
+class nfind
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
@@ -22,7 +22,7 @@ class find1
 				break;
 			}
 			else if(i==n)
-				System.out.println("no");
+				System.out.println("yes");
 			else
 			{
 				
