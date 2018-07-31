a2711aab25a3f734f0a8d65cb78a9455dab26b64
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-07-31 11:43:59 +0530

    Update and rename rangen.java to countint.java

--- countint.java
+++ countint.java
@@ -5,15 +5,19 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class rangen
+class countint
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
-		if(n>=0 && n<=9)
-			System.out.println("yes");
-		else
-			System.out.println("no");
+		int rem=0,count=0;
+		while(n>0)
+		{
+			rem=n%10;
+			count++;
+			n=n/10;
+		}
+		System.out.println(count);
 	}
 }
