90de2be51e51c2418ad109af3d772a3688b5f3f6
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-07-31 10:12:14 +0530

    Update and rename maximum.java to nfibonacci.java

--- nfibonacci.java
+++ nfibonacci.java
@@ -5,28 +5,21 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class maximumnum
+class nfibonacci
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
-		int n=10,t=0;
-		int[] a=new int[n];
-		for(int i=0;i<n;i++)
-			a[i]=sc.nextInt();
-		for(int i=0;i<n;i++)
+		int n=sc.nextInt();
+		int a=1;
+		int b=1,i=0;
+		while(n>i)
 		{
-			for(int j=i+1;j<n;j++)
-			{
-				if(a[i]>a[j])
-				{
-					t=a[i];
-					a[i]=a[j];
-					a[j]=t;
-				}
-			break;
-			}
+			System.out.print(a+" ");
+			int c=a+b;
+			a=b;
+			b=c;
+			i++;
 		}
-		System.out.println(a[9]);
 	}
 }
