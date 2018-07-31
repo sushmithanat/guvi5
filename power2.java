41a2d6f194d6ac13e326beea05f8ceda3f0d09b6
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-07-31 14:19:37 +0530

    Create power2.java

---
+++ power2.java
@@ -0,0 +1,17 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class power2
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		n *= 2;
+		System.out.println(n);
+	}
+}
