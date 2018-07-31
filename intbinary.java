3cc53854a97be13b1fd7731b86a171de7c36b94f
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-07-31 12:09:10 +0530

    Update and rename nextnum.java to intbinary.java

--- intbinary.java
+++ intbinary.java
@@ -5,12 +5,23 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class nextnum
+class intbinary
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
-		System.out.println(++n);
+		String str=sc.nextLine();
+		int count=0;
+		for(int i=0;i<str.length();i++)
+		{
+			if(str.charAt(i)=='1' || str.charAt(i)=='0')
+				count++;
+				
+		}
+		if(count==str.length())
+			System.out.println("yes");
+		else
+			System.out.println("no");
+			
 	}
 }
