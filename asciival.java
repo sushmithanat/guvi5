b8feb37ee6092ec2fefecb9dcfeb26530076988e
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-08-01 16:02:14 +0530

    Create asciival.java

---
+++ asciival.java
@@ -0,0 +1,17 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class asciival
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		char c=sc.next().charAt(0);
+		int a=c;
+			System.out.println(a);
+	
+	}
+}
