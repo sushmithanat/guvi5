8cca2d43baa66edecf41ba76d671bdd5ca429df5
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-08-03 14:05:10 +0530

    Create float5.java

---
+++ float5.java
@@ -0,0 +1,18 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class float5
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		float num1=sc.nextFloat();
+		float num2=sc.nextFloat();
+		float pro=num1*num2;
+		System.out.printf("%.5f",pro);
+	}
+}
