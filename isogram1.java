d11774dd2c9ada3ac569edcd26993bb1d90ac051
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-08-04 21:14:50 +0530

    Update and rename isgram.java to isogram1.java

--- isogram1.java
+++ isogram1.java
@@ -5,7 +5,7 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class isogram
+class isogram1
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
