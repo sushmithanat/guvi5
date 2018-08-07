f479743618a9449f17d57d11c3ffcbef45cf4ae8
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-08-07 22:31:05 +0530

    Create printkth.java

---
+++ printkth.java
@@ -0,0 +1,33 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class printkth
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int k=sc.nextInt();
+		int i=0;
+		int[] a=new int[n];
+		for(i=0;i<n;i++)
+			a[i]=sc.nextInt();
+		for(i=0;i<n;i++)
+		{
+			for(int j=i+1;j<n;j++)
+			{
+				if(a[i]>a[j])
+				{
+					int tmp=a[i];
+					a[i]=a[j];
+					a[j]=tmp;
+				}
+			}
+		}
+			System.out.println(a[--k]);
+	}
+}
