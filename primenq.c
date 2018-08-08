77ba4c29db73cc32c342266027c183f8387f133a
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-08-08 07:26:57 +0530

    Create primenq.c

---
+++ primenq.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+	int n,q,i=0,j=0,count=0;
+	scanf("%d",&n);
+	scanf("%d",&q);
+	j=n+1;
+	while(j<q)
+	{
+		count=0;
+		for(i=2;i<=j/2;i++)
+		{
+			if(j%i==0)
+			{
+				count=1;
+				break;
+			}
+		}
+		if(count==0)
+		{
+			printf("%d ",j);
+		}
+		j++;
+	}
+	return 0;
+}
