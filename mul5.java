7c2416c9b9ea7380704ce4998355215c28d86f90
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-08-08 11:40:41 +0530

    Create mul5.java

---
+++ mul5.java
@@ -0,0 +1,20 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class mul5
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=1;i<6;i++)
+		{
+			int mul=n*i;
+			System.out.print(mul+" ");
+		}
+	}
+}
