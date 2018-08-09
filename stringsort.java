ce74bb37a480d1db34a8fbb92a08108494d9e1d5
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-08-09 10:33:09 +0530

    Create stringsort.java

---
+++ stringsort.java
@@ -0,0 +1,18 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class stringsort
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		char c[]=str.toCharArray();
+		Arrays.sort(c);
+		System.out.println(c);
+	}
+}
