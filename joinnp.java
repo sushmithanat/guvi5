e685066d00d024df53dd6b962da6fe02c0d55729
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-08-10 20:09:20 +0530

    Create joinnp.java

---
+++ joinnp.java
@@ -0,0 +1,18 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class joinnp
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int p=sc.nextInt();
+		System.out.print(n);
+		System.out.print(p);
+	}
+}
