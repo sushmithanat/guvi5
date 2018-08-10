16493560ec4ae877380276865d4aa79987aeade2
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-08-10 20:05:28 +0530

    Create powerpn.java

---
+++ powerpn.java
@@ -0,0 +1,28 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class powerpn
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int p=sc.nextInt();
+		int n1=n;
+		if(p==0)
+			n1=1;
+		else
+		{
+		while(p>1)
+		{
+			n1=n1*n;
+			p--;
+		}
+		}
+		System.out.println(n1);
+	}
+}
