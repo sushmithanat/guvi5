fda5a68089773eb00f36691b2fb4737e775357b0
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-08-14 14:00:21 +0530

    Create strcnt.java

---
+++ strcnt.java
@@ -0,0 +1,18 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class strcnt
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String str1=sc.next();
+		String str2=sc.next();
+		System.out.println(str1+str2);
+		
+	}
+}
