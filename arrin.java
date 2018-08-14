16d50e95b6397702e84c3a4923bbc9f9b67a835f
sushmithanat <41733975+sushmithanat@users.noreply.github.com>
2018-08-14 12:35:10 +0530

    Create arrin.java

---
+++ arrin.java
@@ -0,0 +1,25 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class arrin
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int[] a=new int[n];
+		for(int i=0;i<n;i++)
+			a[i]=sc.nextInt();
+		for(int i=0;i<n;i++)
+		{
+			if(i==n-1)
+				System.out.print(a[i]+" "+i);
+			else
+				System.out.println(a[i]+" "+i);
+		}
+	}
+}
